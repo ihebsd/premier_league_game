794d96c2f391025862521a5dd8d58cfbb90a330d
ihebsd <47121168+ihebsd@users.noreply.github.com>
2025-11-07 18:14:45 +0100

    Add new commit

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card5', img: 'images/success.png' },
+        { name: 'card1', img: 'images/Tottenham.png' },
+        { name: 'card1', img: 'images/Tottenham.png' },
+        { name: 'card2', img: 'images/Arsenal.png' },
+        { name: 'card2', img: 'images/Arsenal.png' },
+        { name: 'card3', img: 'images/Newcastle.png' },
+        { name: 'card3', img: 'images/Newcastle.png' },
+        { name: 'card4', img: 'images/United.png' },
+        { name: 'card4', img: 'images/United.png' },
+        { name: 'card5', img: 'images/Wolves.png' },
+        { name: 'card5', img: 'images/Wolves.png' },
         // ...add more pairs as needed
     ];
 
